728b18fbe893bf84acc8e633e8cd843c47776170
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-03-08 15:34:33 +0100

    Added runtime dockerfile and some other work

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ var http = require('http')
 var _    = require('lodash')
 var argv = require('optimist').argv
 
-console.log(argv)
+var arguments = {
+    name : {
+        validate : function(value) { return value.length > 0 },
+    }
+}
 
 var validate_input = function(prop, value) {
-    console.log(prop, value);
-    return true
+    switch (prop) {
+        case 'name':
+            return value.length > 0
+        case 'target':
+            return value.length > 0
+        case 'ttl':
+            return typeof value === 'number' && value > 0
+    }
 };
 
 ['name','target','ttl'].forEach(function(req_prop) {
@@ -15,10 +25,12 @@ var validate_input = function(prop, value) {
         process.exit(1)
     }
     if (!validate_input(req_prop, argv[req_prop])) {
-        console.log('Invalid property: '+req_prop)
+        console.log('Invalid property value: '+req_prop+' '+argv[req_prop])
         process.exit(1)
     }
 })
+
+console.log('ready to query skydns')
 
 // var ip_pattern = new RegExp(/([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}|(\d{1,3}\.){3}\d{1,3}/)
 // var match_ip = '192.168.0.1'.match(ip_pattern)
